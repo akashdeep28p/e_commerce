452c26b420e6428b363e27bc84f1563517ada070
Akash <akashyap@lenovo.com>
2022-03-07 19:28:04 +0530

    Added logic to register user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,24 @@
+const userModel = require('../models/user')
+const {StatusCodes} = require('http-status-codes')
+const error = require('../errors')
+
 const register = async (req, res) => {
-    res.send('register user')
+    const {email, name, password} = req.body
+    const emailAlreadyInUse = await userModel.findOne({email})
+
+    if(emailAlreadyInUse)
+        throw new error.BadRequestError('Email already in use')
+
+    const isFirstAccount = (await userModel.count({})) === 0
+    const role = isFirstAccount ? 'admin' : 'user'
+
+    const user = await userModel.create({email, name, password, role})
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 const login = async (req, res) => {
     res.send('login user')
+
 }
 
 const logout = async (req, res) => {
